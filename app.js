49d09e96a9d57583eb5a7fcac18156e54accd803
Jason Vinson <vinsonizer@gmail.com>
2022-04-13 11:27:32 -0400

    adding ao list commands

--- app.js
+++ app.js
@@ -1,59 +1,58 @@
-const { App } = require("@slack/bolt");
+const { App, LogLevel } = require("@slack/bolt");
+const { WebClient } = require("@slack/web-api");
+const schedule = require("node-schedule");
+const q = require("./lib/commands/q");
 
 require("dotenv").config();
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.APP_TOKEN,
+  logLevel: LogLevel.INFO,
 });
+
+const token = process.env.SLACK_BOT_TOKEN;
+
+const web = new WebClient(token);
 
 app.command("/q", async ({ command, ack, say }) => {
   try {
     await ack();
-    say("I did the things");
+    switch (command.text) {
+      case "list":
+        let AoList = await q.getTomorrowsAOs();
+        say(JSON.stringify(AoList));
+        break;
+      default:
+        say("Unrecognized command!");
+    }
   } catch (error) {
     console.error(error);
   }
 });
 
-app.message(/hey/, async ({ message, say }) => {
-  try {
-    await say({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `Hey there <@${message.user}>!`,
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Click Me",
-            },
-            action_id: "button_click",
-          },
-        },
-      ],
-      text: `Hey there <@${message.user}>!`,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-app.action("button_click", async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
+// Scheduled tasks for updates
+async function scheduleTasks() {
+  const everyFive = schedule.scheduleJob(
+    "30 16 * * *",
+    async function (fireDate) {
+      console.log(`send at 16:30`);
+      await web.chat.postMessage({
+        text: "it is 16:30",
+        channel: "#slack-tech",
+      });
+    }
+  );
+}
 
 (async () => {
   const port = process.env.PORT || 3000;
   // Start your app
   await app.start(port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+
+  //await scheduleTasks();
 })();
